9ba7e862decdc2825a72ccc28ad2263863b26562
Josh De Leeuw <jmd496@cornell.edu>
2023-03-17 16:13:58 -0400

    Implement Piece Moving

--- _build/default/README.md
+++
@@ -1,4 +0,0 @@
-Josh De Leeuw (jmd496)
-Emmanuel Dodoo, end25
-Jiaxin Li (JL2726)
-Alex Giang (akg72)
--- _build/default/test/main.ml
+++
@@ -1,4 +0,0 @@
-open OUnit2
-
-let suite = "test suite for project" >::: List.flatten []
-let _ = run_test_tt_main suite
