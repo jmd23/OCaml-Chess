c38b932408a2e1aa237cca4c0033e2b615000a60
Josh De Leeuw <jmd496@cornell.edu>
2023-03-21 11:35:35 -0400

    Add Specs to MLI's in src
    - For board.mli, player.mli

--- src/board.mli
+++ src/board.mli
@@ -6,22 +6,63 @@ type piece =
   | Queen of Player.player
   | King of Player.player
 
+(* The type representing a piece on the board, contains information about which
+   player possesses that piece*)
+
 type square =
   | Piece of piece
   | Empty
 
+(* The type representing a square on the board, either a Piece or Empty*)
+
 type board = square list list
+
+(*The type representing a chess board, a 2d list of squares*)
 
 val rank_1 : square list
 val rank_2 : square list
 val rank_7 : square list
 val rank_8 : square list
 val empty_rank : square list
+
+(*rank_1, rank_2, rank_7, and rank_8 are lists of squares, representing ranks of
+  the starting board*)
+
 val piece_to_string : piece -> string
+
+(*[piece_to_string p] returns the string representation, in unicode, of piece p,
+  with the proper color.*)
+
 val print_square : square -> string
+
+(*[print_square s] returns the string representation of a square, which can be a
+  piece or Empty.*)
+
 val row_to_string : int -> square list -> string
+
+(*[row_to_string i s] returns the string representation of row i on the
+  chessboard, s, which is a list of squares.*)
+
 val starting_board : square list list
+
+(* starting_board is data that represents the original board*)
+
 val board_to_string : board -> string
+
+(*[board_to_string b] returns the string representation of the entire board,
+  including the rank and file labels*)
+
 val set_row : int -> 'a list -> 'a -> 'a list
+
+(*[set_row i row p] sets the square at index i of square list row to p, and
+  returns the new row. Requires: 0 <= i < length row. This is used primarily as
+  a helper to set_square*)
+
 val set_square : int -> int -> 'a list list -> 'a -> 'a list list
+
+(*[set_square i j board p] sets the square at index i of square list row to p,
+  and returns the new board. Requires: [i, j] is a valid index of board.*)
+
 val get_square : 'a list list -> int -> int -> 'a
+(*[get_square board i j] gets the value of the board at row i, index j.
+  Requires: [i, j] is a valid index of board.*)
--- src/player.mli
+++ src/player.mli
@@ -2,5 +2,13 @@ type player =
   | White
   | Black
 
+(* The type representing a player of the game*)
+
 val switch_player : player -> player
+
+(*[switch_player p] switches the turn and returns the new player. If p is White,
+  the new player is Black, and vice versa*)
+
 val string_of_player : player -> string
+
+(*[string_of_player p] returns the string representation of player p. *)
