10a6d87109ca905ce0346750af329360b921ced7
Jiaxin Li <jl2726@cornell.edu>
2023-04-28 01:17:30 -0400

    initialize test structures other than for State file

--- test/main.ml
+++ test/main.ml
@@ -18,9 +18,51 @@ let set_row_test (name : string) j row value (expected_output : 'a list) : test
     =
   name >:: fun _ -> assert_equal expected_output (Board.set_row j row value)
 
-let board_to_string_test (name : string) (board : Board.board)
+let set_square_test (name : string) i j (board : Board.board) (p : Board.square)
+    (expected_output : Board.square list list) : test =
+  name >:: fun _ -> assert_equal expected_output (Board.set_square i j board p)
+
+(* need to test string in terminal // note to fix later *)
+let print_board_test (name : string) (board : Board.board)
+    (expected_output : unit) : test =
+  name >:: fun _ -> assert_equal expected_output (Board.print_board board)
+
+(* -------------------Movement Tests---------------------- *)
+let move_test (name : string) (board : Board.board) (plr : Player.player)
+    (lst : int list) (expected_output : Movement.result) : test =
+  name >:: fun _ -> assert_equal expected_output (Movement.move board plr lst)
+
+(* -------------------Player Tests---------------------- *)
+let switch_player_test (name : string) (player : Player.player)
+    (expected_output : Player.player) : test =
+  name >:: fun _ -> assert_equal expected_output (Player.switch_player player)
+
+let string_of_player_test (name : string) (player : Player.player)
     (expected_output : string) : test =
-  name >:: fun _ -> assert_equal expected_output (Board.board_to_string board)
+  name >:: fun _ ->
+  assert_equal expected_output (Player.string_of_player player)
+
+(* -------------------State Tests---------------------- *)
+
+(* -------------------Pieces Tests---------------------- *)
+
+(* Knight *)
+let knight_valid_test (name : string) (board : Board.board)
+    (plr : Player.player) (lst : int list) (expected_output : bool) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (Knight.validate_knight_move board plr lst)
+
+(* Pawn *)
+let pawn_valid_test (name : string) (board : Board.board) (plr : Player.player)
+    (lst : int list) (expected_output : bool) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (Pawn.validate_pawn_move board plr lst)
+
+(* Rook *)
+let rook_valid_test (name : string) (board : Board.board) (plr : Player.player)
+    (lst : int list) (expected_output : bool) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (Rook.validate_rook_move board plr lst)
 
 let suite = "test suite for project" >::: List.flatten []
 let _ = run_test_tt_main suite
